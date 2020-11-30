6cf8d0656b49c6060a856374699a2ded4fc51f68
Li <leochen1990@gmail.com>
2020-11-30 10:32:34 +0100

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="compas_skeleton",
-    version="1.0.0",
+    version="1.1.0",
     description="topologically generating branching diagram mesh ",
     long_description=long_description,
     long_description_content_type="text/markdown",
