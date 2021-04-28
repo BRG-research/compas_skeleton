bedfb67a49b573cf021de5b27527612d362c00d4
brgcode <vanmelet@ethz.ch>
2021-04-28 18:31:56 +0200

    fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 # -*- coding: utf-8 -*-
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -6,11 +7,13 @@
 
 import sys
 import os
-
-from sphinx.ext.napoleon.docstring import NumpyDocstring
+import inspect
+import importlib
 
 import sphinx_compas_theme
+from sphinx.ext.napoleon.docstring import NumpyDocstring
 
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../src'))
 
 # -- General configuration ------------------------------------------------
 
@@ -18,11 +21,11 @@ project = "COMPAS Skeleton"
 copyright = "Block Research Group - ETH Zurich"
 author = "Wenqian Yang"
 release = "1.1.1"
-version = ".".join(release.split(".")[0:2])
+version = '.'.join(release.split('.')[0:2])
 
 master_doc = "index"
 source_suffix = [".rst", ]
-templates_path = ["_templates", ]
+templates_path = sphinx_compas_theme.get_autosummary_templates_path()
 exclude_patterns = []
 
 pygments_style   = "sphinx"
@@ -37,25 +40,34 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.doctest",
+    "sphinx.ext.coverage",
+    "sphinx.ext.linkcode",
+    "sphinx.ext.extlinks",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
     "matplotlib.sphinxext.plot_directive",
-    'm2r',
-    'nbsphinx',
-    'sphinx.ext.viewcode'
 ]
 
 # autodoc options
 
-autodoc_default_flags = [
-    "undoc-members",
-    'show-inheritance',
-]
+autodoc_default_options = {
+    "undoc-members": True,
+    "show-inheritance": True,
+}
 
 autodoc_member_order = "alphabetical"
 
 autoclass_content = "class"
+
+def skip(app, what, name, obj, would_skip, options):
+    if name.startswith('_'):
+        return True
+    return would_skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
 
 # autosummary options
 
@@ -63,7 +75,7 @@ autosummary_generate = True
 
 # napoleon options
 
-napoleon_google_docstring = True
+napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = False
 napoleon_include_private_with_doc = False
@@ -75,55 +87,70 @@ napoleon_use_ivar = False
 napoleon_use_param = False
 napoleon_use_rtype = False
 
-
-# first, we define new methods for any new sections and add them to the class
-def parse_keys_section(self, section):
-    return self._format_fields('Keys', self._consume_fields())
-NumpyDocstring._parse_keys_section = parse_keys_section
-
-
-def parse_attributes_section(self, section):
-    return self._format_fields('Attributes', self._consume_fields())
-NumpyDocstring._parse_attributes_section = parse_attributes_section
-
-
-def parse_class_attributes_section(self, section):
-    return self._format_fields('Class Attributes', self._consume_fields())
-NumpyDocstring._parse_class_attributes_section = parse_class_attributes_section
-
-
-# we now patch the parse method to guarantee that the the above methods are
-# assigned to the _section dict
-def patched_parse(self):
-    self._sections['keys'] = self._parse_keys_section
-    self._sections['attributes'] = self._parse_attributes_section
-    self._sections['class attributes'] = self._parse_class_attributes_section
-    self._unpatched_parse()
-NumpyDocstring._unpatched_parse = NumpyDocstring._parse
-NumpyDocstring._parse = patched_parse
-
-
 # plot options
-
-# plot_include_source
-# plot_pre_code
-# plot_basedir
-# plot_formats
-# plot_rcparams
-# plot_apply_rcparams
-# plot_working_directory
-# plot_template
 
 plot_html_show_source_link = False
 plot_html_show_formats = False
+
+# docstring sections
+
+def parse_attributes_section(self, section):
+    return self._format_fields("Attributes", self._consume_fields())
+
+NumpyDocstring._parse_attributes_section = parse_attributes_section
+
+def patched_parse(self):
+    self._sections["attributes"] = self._parse_attributes_section
+    self._unpatched_parse()
+
+NumpyDocstring._unpatched_parse = NumpyDocstring._parse
+NumpyDocstring._parse = patched_parse
 
 # intersphinx options
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/", None),
-    "compas": ("https://compas-dev.github.io/main", "https://compas-dev.github.io/main/objects.inv"),
+    "compas": ("https://compas.dev/compas/latest/", None),
 }
 
+# linkcode
+
+def linkcode_resolve(domain, info):
+    if domain != 'py':
+        return None
+    if not info['module']:
+        return None
+    if not info['fullname']:
+        return None
+
+    package = info['module'].split('.')[0]
+    if not package.startswith('compas_skeleton'):
+        return None
+
+    module = importlib.import_module(info['module'])
+    parts = info['fullname'].split('.')
+
+    if len(parts) == 1:
+        obj = getattr(module, info['fullname'])
+        filename = inspect.getmodule(obj).__name__.replace('.', '/')
+        lineno = inspect.getsourcelines(obj)[1]
+    elif len(parts) == 2:
+        obj_name, attr_name = parts
+        obj = getattr(module, obj_name)
+        attr = getattr(obj, attr_name)
+        if inspect.isfunction(attr):
+            filename = inspect.getmodule(obj).__name__.replace('.', '/')
+            lineno = inspect.getsourcelines(attr)[1]
+        else:
+            return None
+    else:
+        return None
+
+    return f"https://github.com/blockresearchgroup/compas_skeleton/blob/master/src/{filename}.py#L{lineno}"
+
+# extlinks
+
+extlinks = {}
 
 # -- Options for HTML output ----------------------------------------------
 
@@ -131,14 +158,14 @@ html_theme = "compaspkg"
 html_theme_path = sphinx_compas_theme.get_html_theme_path()
 
 html_theme_options = {
-    "package_name"    : "compas_skeleton",
-    'package_title' : project,
-    'package_version' : release,
-    'package_author' : author,
-    'package_description' : '',
-    'package_docs' : 'https://blockresearchgroup.github.io/compas_skeleton/',
-    'package_repo' : 'https://github.com/blockresearchgroup/compas_skeleton/',
-    "package_old_versions_txt" : "https://blockresearchgroup.github.io/compas_skeleton/doc_versions.txt"
+    "package_name": "compas_skeleton",
+    'package_title': project,
+    'package_version': release,
+    'package_author': author,
+    'package_description': '',
+    'package_docs': 'https://blockresearchgroup.github.io/compas_skeleton/',
+    'package_repo': 'https://github.com/blockresearchgroup/compas_skeleton/',
+    "package_old_versions_txt": "https://blockresearchgroup.github.io/compas_skeleton/doc_versions.txt"
 }
 
 html_context = {}
