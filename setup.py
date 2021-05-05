267a2b4b8857e5b69a906bc2ffbd8ac4181bcb79
Li <leochen1990@gmail.com>
2021-05-05 10:02:26 +0200

    Bump version to 1.1.2rc0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="compas_skeleton",
-    version="1.1.1",
+    version="1.1.2rc0",
     description="topologically generating branching diagram mesh ",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- src/compas_skeleton/__init__.py
+++ src/compas_skeleton/__init__.py
@@ -25,7 +25,7 @@ __author__ = ["Wenqian Yang"]
 __copyright__ = "Block Research Group - ETH Zurich"
 __license__ = "MIT License"
 __email__ = "yang@arch.ethz.ch"
-__version__ = "1.1.1"
+__version__ = "1.1.2rc0"
 
 
 HERE = os.path.dirname(__file__)
